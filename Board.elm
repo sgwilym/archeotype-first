f97d07c92df9bb2bb98a3c4d8986bd678cb31042
Samuel Gwilym <sam@comique.co>
2016-09-29 21:33:27 +0200

    Truly fixed board randomisation!

--- Board.elm
+++ Board.elm
@@ -7,6 +7,7 @@ import Attempt
 import Letter exposing (Letter(..))
 import Random
 import String
+import Char
 
 
 type Cell
@@ -29,27 +30,26 @@ cellsFromProblem { answer, attempt } =
                 attemptLetters =
                     case Attempt.last attempt' of
                         Attempt.Failure _ ->
-                              let
-                                  allLetters =
-                                      Attempt.toLetters attempt'
-                              in
-                                  case Cons.toList allLetters of
-                                    [head] ->
-                                       Nothing
+                            let
+                                allLetters =
+                                    Attempt.toLetters attempt'
+                            in
+                                case Cons.toList allLetters of
+                                    [ head ] ->
+                                        Nothing
 
-                                    head::rest ->
-                                      Just (
-                                      cons
-                                          head
-                                          (List.take
-                                              ((List.length rest) - 1)
-                                              rest
-                                          )
-                                          )
+                                    head :: rest ->
+                                        Just
+                                            (cons
+                                                head
+                                                (List.take
+                                                    ((List.length rest) - 1)
+                                                    rest
+                                                )
+                                            )
 
                                     [] ->
-                                      Nothing
-
+                                        Nothing
 
                         Attempt.Success _ _ ->
                             Just (Attempt.toLetters attempt')
@@ -61,20 +61,20 @@ cellsFromProblem { answer, attempt } =
                             Just (Attempt.toLetters attempt')
             in
                 case attemptLetters of
-                  Just letters ->
-                    Cons.indexedMap
-                        (\index letter ->
-                            case index < (Cons.length letters) of
-                                True ->
-                                    EmptyCell
+                    Just letters ->
+                        Cons.indexedMap
+                            (\index letter ->
+                                case index < (Cons.length letters) of
+                                    True ->
+                                        EmptyCell
 
-                                False ->
-                                    Cell letter
-                        )
-                        answer
+                                    False ->
+                                        Cell letter
+                            )
+                            answer
 
-                  Nothing ->
-                    Cons.map (\letter -> Cell letter) answer
+                    Nothing ->
+                        Cons.map (\letter -> Cell letter) answer
 
         Nothing ->
             Cons.map (\letter -> Cell letter) answer
@@ -85,59 +85,53 @@ fromPuzzle puzzle width seed =
     case puzzle of
         InProgress problems ->
             let
-                sortedProblems =
-                    Cons.map fst
-                        (Cons.sortBy snd
-                            (Cons.map2
-                                (,)
-                                problems
-                                (Cons.map
-                                    (\problem ->
-                                        String.fromList
-                                            (Cons.toList
-                                                (Cons.map Letter.toChar problem.answer)
-                                            )
-                                    )
-                                    problems
-                                )
-                            )
-                        )
+                problemToChars =
+                    \problem ->
+                        Cons.map Letter.toChar problem.answer
 
-                cells =
-                    Cons.map cellsFromProblem sortedProblems
-
-                flatCells =
-                    Cons.concat cells
-
-                seed' =
-                    Random.initialSeed seed
-
-                randomCons cellSeed =
+                randomCons consSeed =
                     Random.step
                         (Random.map
                             (\n -> cons n [])
                             (Random.int 0 100)
                         )
-                        (Random.initialSeed
-                            cellSeed
-                        )
+                        (Random.initialSeed consSeed)
 
+                -- [['C', A, T], [D, O, G], [A, P, P, L, E]]
+                problemChars =
+                    Cons.map problemToChars problems
+
+                -- [EmptyCell, EmptyCell, Cell P, Cell P, Cell L, etc... ]
+                problemCells =
+                    Cons.map cellsFromProblem problems
+
+                -- (['A', 'P', 'P', 'L', 'E'] , [EmptyCell, EmptyCell, Cell P, Cell L, Cell E])
+                ( sortedChars, sortedCells ) =
+                    Cons.map2 (,) problemChars problemCells
+                        |> Cons.sortBy (\( chars, _ ) -> String.fromList (Cons.toList chars))
+                        |> Cons.unzip
+
+                flatChars =
+                    Cons.concat sortedChars
+
+                flatCells =
+                    Cons.concat sortedCells
+
+                -- [12, 34, 54, 1, 4, 19, 1, 2, 100, 21, 11]
                 randomValues =
-                    Cons.concat
-                        (Cons.indexedMap
-                            ((\index _ ->
-                                fst (randomCons (seed + index))
-                             )
-                            )
-                            flatCells
+                    Cons.indexedMap
+                        (\index char ->
+                            randomCons (index * Char.toCode char)
                         )
+                        flatChars
+                        |> Cons.map fst
+                        |> Cons.concat
 
+                -- Sort the list of cells by the random values
                 shuffledCells =
-                    (Cons.map fst
-                        (Cons.sortBy snd
-                            (Cons.map2 (,) flatCells randomValues)
-                        )
-                    )
+                    Cons.map2 (,) flatCells randomValues
+                        |> Cons.sortBy snd
+                        |> Cons.map fst
             in
                 { cells = shuffledCells, width = width, seed = seed }
 
